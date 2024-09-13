c74c7685697e6b206ef2465606f74843e2275a67
coburndaniel979 <coburndaniel979@gmail.com>
2024-09-13 17:10:30 -0400

    Add some cryptic looking tables
    Add the matrixRef matrix and the MatrixVals table.

--- src/MatrixManager.java
+++ src/MatrixManager.java
@@ -13,6 +13,28 @@ public class MatrixManager {
     //  - shift
     //  - moving shift?
 
+    // represents the "letters" on the matrix
+    // using a letters row and column we can find its character pair
+    private String[][] matrixRef = {
+            {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"},
+            {"l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"},
+            {"w", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7"},
+            {"8", "9", "0", " ", "a", "b", "c", "d", "e", "f", "g"},
+            {"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r"},
+            {"s", "t", "u", "v", "w", "x", "y", "z", "1", "2", "3"},
+            {"4", "5", "6", "7", "8", "9", "0", " ", "a", "b", "c"},
+            {"d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"},
+            {"o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y"},
+            {"z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"},
+            {"_", "a", "e", "i", "o", "u", "y", " ", "a", "e", "i"}
+    };
+
+    // this represents the characters for the row and column at their index
+    private String[][] matrixVals = {
+            {"1", "3", "7", "A", "%", "@", "&", "g", "{", ">", "#"},
+            {"2", "5", "C", "N", ")", ">", "<", "G", "!", "(", "]"}
+    };
+
     public static String getFirstChar(char letter, int randNum) {
         return "NULL";
     }
