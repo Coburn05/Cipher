9b2d53e63eb4fef14fbd82e6e208e8f89149cdc9
coburndaniel979 <coburndaniel979@gmail.com>
2024-10-06 12:15:24 -0400

    rsa things, need to make sure it is single responsibility

--- src/RSAHandler.java
+++ src/RSAHandler.java
@@ -1,6 +1,28 @@
 package src;
 
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
 public class RSAHandler {
-    // handles RSA key generation
-    // DOES NOT DO ANY ENCRYPTION OR DECRYPTION
+    // does ALL RSA related things
+    // will break apart later
+    private static long p;
+    private static long q;
+    private static long n; // p * q
+    private static long phi; // (p-1)(q-1)
+    private static long e; // 1<e<phi
+
+    private static double gcd(double a, double h) {
+        double tmp;
+        while(true) {
+            tmp = a % h;
+            if(tmp == 0)
+                return h;
+            a = h;
+            h = tmp;
+        }
+    }
+
+    private static String encryptRSA
 }
